e9eed2e2bd1afddf495f8d6c2fa8c616bd463a3e
lissa <e-namestnikova@inbox.ru>
2022-08-09 16:04:41 +0700

    edit code

--- task/Program.cs
+++ task/Program.cs
@@ -5,25 +5,28 @@ string[] ParseArray(string input, char separator)
 {
     string[] resultElements = new string[GetCountElementsInString(input)];
     int index = 0;
-    string subString = String.Empty;
+    string buffString = String.Empty;
     int length = 0;
     for (int i = 0; i < input.Length; i++)
     {
         if (input[i] == separator)
         {
-            resultElements[index++] = subString;
-            length = subString.Length;
-            if (length <= 3) Console.Write($"'{subString}' ");
-            subString = String.Empty;
+            resultElements[index++] = buffString
+            ;
+            length = buffString
+            .Length;
+            if (length <= 3) Console.Write($"'{buffString}' ");
+            buffString
+             = String.Empty;
         }
         else
         {
-            subString += input[i];
+            buffString += input[i];
         }
     }
-    resultElements[index] = subString;
-    length = subString.Length;
-    if (length <= 3) Console.Write($"'{subString}' ");
+    resultElements[index] = buffString;
+    length = buffString.Length;
+    if (length <= 3) Console.Write($"'{buffString}' ");
     return resultElements;
 }
 int GetCountElementsInString(string elements)
